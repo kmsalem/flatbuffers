3421c161f95f55515158e87e410629521e8c5539
Yuxiao Yu <SYN\y257yu@white.syn.uwaterloo.ca>
2018-07-20 10:24:05 -0400

    Update script for main experiments

--- comparison-ramp/node1.sh
+++ comparison-ramp/node1.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 server_id=0
-test_memory=$2
+num_entries=$2
+test_memory=$3
 output=$1_output.txt
 
 if [ -f $output ] ; then
@@ -10,8 +11,7 @@ fi
 
 for ((i=1; i <=10; i++))
 do
-    echo "***********************new test start********************"
     echo $i >> $output
-	./$1 config.txt $server_id $test_memory >> $output
+	./$1 config.txt $server_id $num_entries $test_memory >> $output
     sleep 1
 done
--- comparison-ramp/node2.sh
+++ comparison-ramp/node2.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 server_id=1
-test_memory=$2
+num_entries=$2
+test_memory=$3
 
 for ((i=1; i <=10; i++))
-do
-	./$1 config.txt $server_id $test_memory
+do 
+	./$1 config.txt $server_id $num_entries $test_memory
     sleep 3
 done
