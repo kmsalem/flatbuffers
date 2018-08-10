1c1a8903ee5d3048a037f0a52a86713b77a692f0
Yuxiao Yu <y257yu@aqua08.syn.uwaterloo.ca>
2018-08-10 16:00:08 -0400

    Update experiment

--- experiment-ramp/map_experiment.cpp
+++ experiment-ramp/map_experiment.cpp
@@ -1,7 +1,5 @@
 #include <iostream>
 #include <utility>
-#include "flatbuffers/idl.h"
-#include "flatbuffers/util.h"
 #include "map_experiment_generated.h"  // includes "flatbuffer_ramp.h"
 #include "flatbuffers/ramp_builder.h"
 #include "flatbuffers/SAllocator.h"
